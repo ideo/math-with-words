c66b471458f12962590fb8e1e772b5d7a8bd756e
Joseph Gambino <joe@gam.bingo>
2020-10-06 22:14:09 +0900

    whoopsie

--- setup.sh
+++ setup.sh
@@ -11,10 +11,3 @@ headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
 " > ~/.streamlit/config.toml
-
-echo "\
-[default]\n\
-aws_access_key_id = AKIAIIOWKDN6GPSORW2Q\n\
-aws_secret_access_key = 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\n\
-region=ap-northeast-1\n\
-" > ~~.aws/credentials
